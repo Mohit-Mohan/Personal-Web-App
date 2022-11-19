1fbe4c16152aef55c63d7e11335bebec3758f806
ishsarin2000 <ish.sarin@imaginemarketingindia.com>
2022-11-20 00:25:28 +0530

    tandem bicyle img

--- src/components/pages/TandemBicycle.js
+++ src/components/pages/TandemBicycle.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import FEA2 from "../../media/FEA2.png";
+
 const TandemBicycle = () => {
   return (
     <div className="project1">
@@ -15,14 +17,22 @@ const TandemBicycle = () => {
             />
           </div>
           <div class="col-6 starting_text ">
-          /imagine A Double headed dragon with mesh like detailed scales, one body, visually stunning, gradient mesh like colours, intricate, colour graded, 8k --ar 16:9
+            /imagine A Double headed dragon with mesh like detailed scales, one
+            body, visually stunning, gradient mesh like colours, intricate,
+            colour graded, 8k --ar 16:9
           </div>
         </div>
       </div>
 
       <div className="tb_img1" />
       <div className="brief_text_white_bg">
-      This project aims to navigate through the design process of a lightweight and adult-friendly tandem bicycle frame with two seats using the finite element method on Solidworks 2021. It also details the processes of mesh refinement, model sanity checks, determination of fatigue life, as well as the improvement of their natural frequencies using FEA, which were undertaken for the successful completion of the project.
+        This project aims to navigate through the design process of a
+        lightweight and adult-friendly tandem bicycle frame with two seats using
+        the finite element method on Solidworks 2021. It also details the
+        processes of mesh refinement, model sanity checks, determination of
+        fatigue life, as well as the improvement of their natural frequencies
+        using FEA, which were undertaken for the successful completion of the
+        project.
       </div>
       <div className="tb_img2" src="" alt="" />
 
@@ -31,34 +41,29 @@ const TandemBicycle = () => {
           <img
             className="img-fluid"
             src="https://i.ibb.co/gvQrg14/FEA1.png"
-            
-           
             border="0"
           />
         </div>
-      
+
         <div>
-          <img
+          <img src={FEA2} alt="hello" srcset="" />
+          {/* <img
             className="img-fluid"
            
             src="https://i.ibb.co/KrcXLhz/FEA2.png"
             
             border="0"
-          />
+          /> */}
         </div>
 
-        
         <div>
           <img
             className=" img-fluid"
             src="https://i.ibb.co/840c3Jg/FEA3.png"
-            
             border="0"
             // style={{ display: "block", marginLeft: "3%" }}
           />
         </div>
-
-       
       </div>
       <Link to="/" className="homepage_btn ">
         Home Page
